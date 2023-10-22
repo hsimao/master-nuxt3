e4bd87b791236265217878b35af744d5424a738e
hsimao <e087754958@gmail.com>
2023-10-22 23:30:14 +0800

    feat: 1-4 dynamic routes

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>{{ chapter && chapter.title }}</p>
+  <p>{{ lesson && lesson.title }}</p>
+</template>
+
+<script setup lang="ts">
+const { params } = useRoute()
+const course = useCourse()
+console.log('course', course)
+
+const chapter = computed(() => {
+  return (
+    course.chapters.find((chapter) => chapter.slug === params.chapterSlug) ||
+    false
+  )
+})
+
+const lesson = computed(() => {
+  if (!chapter.value) return false
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === params.lessonSlug
+  )
+})
+console.log('lesson', lesson.value)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/course/lesson.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>nested lesson</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
