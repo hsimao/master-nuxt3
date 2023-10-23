946517ea73cea5bddb8aaf69d6a7627de18b706a
hsimao <e087754958@gmail.com>
2023-10-23 16:57:11 +0800

    feat: nuxtlink active style

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,5 +1,13 @@
 import courseData from './courseData'
 
 export const useCourse = () => {
-  return courseData
+  const chapters = courseData.chapters.map((chapter) => ({
+    ...chapter,
+    lessons: chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`
+    }))
+  }))
+
+  return { ...courseData, chapters }
 }
--- pages/course.vue
+++ pages/course.vue
@@ -25,8 +25,12 @@
           <NuxtLink
             v-for="(lesson, index) in chapter.lessons"
             :key="lesson.slug"
+            :to="lesson.path"
             class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath
+            }"
           >
             <span class="text-gray-500">{{ index + 1 }}.</span>
             <span>{{ lesson.title }}</span>
@@ -44,3 +48,5 @@
 <script setup lang="ts">
 const { chapters } = useCourse()
 </script>
+
+<style lang="scss" scoped></style>
