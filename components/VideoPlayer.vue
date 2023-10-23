3792c39c91fe7409a0069195fc6b5b375965046a
hsimao <e087754958@gmail.com>
2023-10-23 16:25:00 +0800

    feat: add videoPlayer

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    width="500"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<style scoped></style>
+
+<script setup lang="ts">
+const props = defineProps({
+  videoId: {
+    type: String,
+    required: true
+  }
+})
+</script>
