c8caace3bc3342c23267591a848315da24b30dbb
hsimao <e087754958@gmail.com>
2023-10-24 18:17:09 +0800

    feat: add course type

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,7 +1,30 @@
 import courseData from './courseData'
 
-export const useCourse = () => {
-  const chapters = courseData.chapters.map((chapter) => ({
+export interface Lesson {
+  title: string
+  slug: string
+  number: number
+  downloadUrl: string
+  sourceUrl?: string
+  videoId: number
+  text: string
+  path: string
+}
+
+export interface Chapter {
+  title: string
+  slug: string
+  number: number
+  lessons: Lesson[]
+}
+
+export interface Course {
+  title: string
+  chapters: Chapter[]
+}
+
+export const useCourse = (): Course => {
+  const chapters: Chapter[] = courseData.chapters.map((chapter) => ({
     ...chapter,
     lessons: chapter.lessons.map((lesson) => ({
       ...lesson,
