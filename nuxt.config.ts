1f0d6805a887544e08505249795c575d04ae6f98
hsimao <e087754958@gmail.com>
2023-10-27 19:46:18 +0800

    feat: add supbase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true }
 })
