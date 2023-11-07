313eb417e2a7e324cf29369f87a59cb11ad0043f
hsimao <e087754958@gmail.com>
2023-11-07 23:04:50 +0800

    feat: calculate complete progress and show course and chapter progress

--- stores/useCourseProgress.ts
+++ stores/useCourseProgress.ts
@@ -60,5 +60,35 @@ export const useCourseProgress = defineStore('courseProgress', () => {
     }
   }
 
-  return { initialize, progress, toggleComplete }
+  // Calculate complete progress
+  const percentageCompleted = computed(() => {
+    const chapters = Object.values(progress.value).map((chapter) => {
+      const lessons = Object.values(chapter)
+      const completedLessons = lessons.filter((lesson) => lesson)
+
+      return Number((completedLessons.length / lessons.length) * 100).toFixed(0)
+    })
+
+    const totalLessons = Object.values(progress.value).reduce(
+      (number, chapter) => {
+        return number + Object.values(chapter).length
+      },
+      0
+    )
+
+    const totalCompletedLessons = Object.values(progress.value).reduce(
+      (number, chapter) => {
+        return number + Object.values(chapter).filter((lesson) => lesson).length
+      },
+      0
+    )
+
+    const course = Number((totalCompletedLessons / totalLessons) * 100).toFixed(
+      0
+    )
+
+    return { chapters, course }
+  })
+
+  return { initialize, progress, toggleComplete, percentageCompleted }
 })
